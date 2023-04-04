462c4a3f9d237cf6a1a93cad0c459447847dab02
PillageDev <pillageyt@gmail.com>
2023-04-04 00:36:36 -0400

    Create preset + progress on database
    Co-authored-by: Incbom <IncbomDev@users.noreply.github.com>

--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@ class Data{
         this.data = new Map();
         this.UNWANTED_FIELDS = [];
         this.score = new Map();
+        this.presetFields = [];
     }
 
     initTeam(num){
@@ -66,45 +67,93 @@ class Data{
 
     teamScore(team) {
         var fields = this.fields();
-        for (var i = 0; i <= fields.length; i++) {
+        for (var i = 0; i < fields.length; i++) {
             //parse out unwanted fields
-            for(var j = 0; i < this.UNWANTED_FIELDS.length; j++) {
+            for(var j = 0; j < this.UNWANTED_FIELDS.length; j++) {
                 if(fields[i] == this.UNWANTED_FIELDS[j]) {
                     fields.splice(i, 1);
                 }
             } 
             //do calculations
-            var totalScore;
-            while (i <= fields.length) {
-                totalScore += fields[i];
+            var totalScore = 0;
+            while (i < fields.length) {
+                totalScore += this.getVal(team, fields[i]);
+                i++;
             }
             this.score.get(team).set("Total Score", totalScore);
         }
     }
 
+
     createField() {
         var button = document.getElementById("createField");
+        var self = this;
         button.addEventListener("click", function() {
             console.log("button clicked");
-            var presetFields = [];
-            newField = prompt("Enter a field name");
-            if (newField == null || newField == "") {
+            self.newField = prompt("Enter a field name");
+            if (self.newField == null || self.newField == "") {
                 alert("Invalid field name");
             } else {
-                newFieldType = prompt("Enter a field type (number, text, true/false)");
-                if (newFieldType == null || newFieldType == "") {
+                self.newFieldType = prompt("Enter a field type (number, text, true/false)");
+                if (self.newFieldType == null || self.newFieldType == "") {
                     alert("Invalid field type");
-                } else if (newFieldType.toLowerCase() == "number") {
-                    presetFields.push({name: newField, type: "number"});
-                } else if (newFieldType.toLowerCase() == "text") {
-                    presetFields.push({name: newField, type: "text"});
-                } else if (newFieldType.toLowerCase() == "true/false" || newFieldType.toLowerCase() == "true / false") {
-                    presetFields.push({name: newField, type: "checkbox"});
+                } else if (self.newFieldType.toLowerCase() == "number") {
+                    self.presetFields.push({name: self.newField, type: "number"});
+                } else if (self.newFieldType.toLowerCase() == "text") {
+                    self.presetFields.push({name: self.newField, type: "text"});
+                } else if (self.newFieldType.toLowerCase() == "true/false" || self.newFieldType.toLowerCase() == "true / false") {
+                    self.presetFields.push({name: self.newField, type: "checkbox"});
+                } else {
+                    alert("Invalid field type");
+                }
+                
+            }
+        console.log(self.presetFields);
+        });
+    }
+    
+    applyPresetData() {
+        var button = document.getElementById("applyPresetDataButton");
+        var self = this;
+        button.addEventListener("click", function() {
+            console.log("button clicked");
+            console.log(self.presetFields);
+            if(self.presetFields == null || self.presetFields.length == 0) {
+                self.newField = prompt("Enter a field name");
+                if (self.newField == null || self.newField == "") {
+                    alert("Invalid field name");
+                } else {
+                    self.newFieldType = prompt("Enter a field type (number, text, true/false)");
+                    if (self.newFieldType == null || self.newFieldType == "") {
+                        alert("Invalid field type");
+                    } else if (self.newFieldType.toLowerCase() == "number") {
+                        self.presetFields.push({name: self.newField, type: "number"});
+                    } else if (self.newFieldType.toLowerCase() == "text") {
+                        self.presetFields.push({name: self.newField, type: "text"});
+                    } else if (self.newFieldType.toLowerCase() == "true/false" || self.newFieldType.toLowerCase() == "true / false") {
+                        self.presetFields.push({name: self.newField, type: "checkbox"});
+                    } else {
+                        alert("Invalid field type");
+                    }  
+                }
+            console.log(self.presetFields);
+            } else {
+                for (var i = 0; i < self.presetFields.length; i++) {
+                    self.addField(self.presetFields[i].name);
+                    console.log(self.presetFields[i].name);
                 }
             }
         });
-    }
+    }    
+}    
+
+window.onload = function(){
+    var data = new Data("Data");
+    data.createField();
+    data.applyPresetData([]);
 }
+
+
 
 var Datasets = [];
 var TestSet = new Data("Test Set");
--- js/database.js
+++ js/database.js
@@ -1,18 +1,18 @@
-const ws = new WebSocket('ws://localhost:8080/websocket');
-
-ws.on('connection', function connection(ws) {
+const ws = new WebSocket('ws://localhost:5555');
+ws.addEventListener('error', (event) => {
+  console.error('WebSocket error:', event);
+});
+ws.addEventListener('open', function() {
   console.log('New client connected');
 
-  // Send a message to the client on connection
+  // Send a message to the server on connection
   ws.send('Welcome to the server');
-
-  // Listen for messages from the client
-  ws.on('message', function incoming(message) {
-    console.log(`Received message from client: ${message}`);
-  });
 });
 
-
+// Listen for messages from the server
+ws.addEventListener('message', function(event) {
+  console.log(`Received message from server: ${event.data}`);
+});
 
 function addData(data) {
   const newTeamData = {
@@ -23,31 +23,69 @@ function addData(data) {
     }
   }
 
-  // Send a POST request to the server-side PHP script
-  fetch('../add_data.php', {
-    method: 'POST',
-    body: JSON.stringify(newTeamData),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log(data.message);
-  })
-  .catch(error => {
-    console.error(error);
-  });
-  ws.send("Data added");
-}
+  if (ws.readyState === WebSocket.OPEN) {
+    // If the WebSocket connection is open, send the message
+    ws.send(JSON.stringify({ action: 'add', data: newTeamData }));
+    
+    // Send a POST request to the server-side PHP script
+    fetch('../add_data.php', {
+      method: 'POST',
+      body: JSON.stringify(newTeamData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data.message);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+  } else {
+    // If the WebSocket connection is still connecting, wait for the 'open' event
+    ws.addEventListener('open', () => {
+      ws.send(JSON.stringify({ action: 'add', data: newTeamData }));
 
+      // Send a POST request to the server-side PHP script
+      fetch('../add_data.php', {
+        method: 'POST',
+        body: JSON.stringify(newTeamData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.message);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    });
+  }
+}
 
 
 function editData(teamNumber, newData) {
-  // Send a POST request to the server-side PHP script to update the data for the team
+  const updatedData = {
+    [teamNumber]: newData
+  }
+
+  if (ws.readyState === WebSocket.OPEN) {
+    // If the WebSocket connection is open, send the message
+    ws.send(JSON.stringify(updatedData));
+  } else {
+    // If the WebSocket connection is still connecting, wait for the 'open' event
+    ws.addEventListener('open', () => {
+      ws.send(JSON.stringify(updatedData));
+    });
+  }
+
+  // Send a POST request to the server-side PHP script
   fetch('../edit_data.php', {
     method: 'POST',
-    body: JSON.stringify({ team: teamNumber, data: newData }),
+    body: JSON.stringify(updatedData),
     headers: {
       'Content-Type': 'application/json'
     }
@@ -59,6 +97,14 @@ function editData(teamNumber, newData) {
   .catch(error => {
     console.error(error);
   });
-  ws.send("Data edited");
 }
+
+
+const data = {
+  teamNumber: 293,
+  canAutoBalance: true,
+  notes: 'This team is awesome!'
+}
+
+addData(data);
 export default ws;
