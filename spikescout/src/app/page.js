8cd359e863c6d64f87bd4ea3a7236fb371758885
PillageDev <63556914+PillageDev@users.noreply.github.com>
2023-10-11 17:50:28 +0000

    Homepage work

--- spikescout/src/app/page.js
+++ spikescout/src/app/page.js
@@ -1,6 +1,7 @@
 "use client"
 import '../styles/globals.css'
 import styles from '../styles/page.module.css'
+import Image from "next/image"
 
 export default function Main() {
     return (
@@ -14,6 +15,34 @@ export default function Main() {
                     <a href="/"><h2>About us</h2></a>
                     <a href="/"><h2>Login</h2></a>
                     <button id={styles.signup}>Sign up</button>
+                </div>
+                <div id={styles.container}>
+                    <div id={styles.contLeft}> {/* pulled a funny and made the left side the right side */}
+                    <Image src="/main.svg" width={500} height={500} />
+                    </div>
+                    <div id={styles.contRight}>
+                        <div id={styles.textContainer}>
+                            <h1>Your warehouse for all things FRC.</h1>
+                            <h2>Manage your team, store your data, supercharge your gameplay.</h2>
+                            <button id={styles.learnMore}>Learn More</button>
+                        </div>
+                    </div>
+                </div>
+                <div id={styles.main}>
+                    <div id={styles.infoHero}>
+                        <div id={styles.heroContainer}>
+                            <h1>How we can help you</h1>
+                            <div class={styles.number}>
+                                <h2>1</h2>
+                            </div>
+                            <div class={styles.number}>
+                                <h2>2</h2>
+                            </div>
+                            <div class={styles.number}>
+                                <h2>3</h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
